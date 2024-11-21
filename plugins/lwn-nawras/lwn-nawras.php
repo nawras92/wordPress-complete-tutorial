08cc04e1fd15c724df0f08ad18cc57ab5431b633
Nawras <nawras.othman@yandex.com>
2024-11-21 10:21:14 +0300

    add first empty plugin

---
+++ plugins/lwn-nawras/lwn-nawras.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Plugin Name: LWN Nawras First Plugin
+ * Description: My first plugin
+ * Version:  1.0.0
+ * Requires at least: 6.7
+ * Requires PHP: 8.0
+ * Author: Nawras Ali
+ * License: GPLv2 or later
+ */
+
