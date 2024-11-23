7ccb6a3204ed7a7aded0f17f99c32dcb77819c5d
Nawras <nawras.othman@yandex.com>
2024-11-23 18:13:02 +0300

    add admin notices plugin

--- plugins/lwn-nawras/lwn-nawras.php
+++ plugins/lwn-nawras/lwn-nawras.php
@@ -8,5 +8,20 @@
  * Requires PHP: 8.0
  * Author: Nawras Ali
  * License: GPLv2 or later
+ * Text Domain: lwn-nawras
  */
 
+/* admin_notices */
+
+add_action('admin_notices', 'lwn_nawras_my_first_message');
+function lwn_nawras_my_first_message()
+{
+  $output = "<div class='notice notice-success'>";
+  $output .= '<p>';
+  $output .= __('Welcome to your first plugin', 'lwn-nawras');
+  $output .= '</p>';
+  $output .= '</div>';
+  echo $output;
+}
+
+/* my_first_message(); */
