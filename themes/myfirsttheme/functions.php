36cb6b94be4af21b1e54c978516e20e8854c0073
Nawras <nawras.othman@yandex.com>
2024-11-28 12:31:31 +0300

    enqueue scripts in theme

--- themes/myfirsttheme/functions.php
+++ themes/myfirsttheme/functions.php
@@ -1,5 +1,13 @@
 <?php
 
+//init
+/* add_action('init', 'quick_func'); */
+/* function quick_func() */
+/* { */
+/*   /1* echo get_template_directory_uri(); *1/ */
+/*   echo get_template_directory(); */
+/* } */
+
 // add styles
 add_action('wp_enqueue_scripts', 'myfirsttheme_add_stylesheets');
 function myfirsttheme_add_stylesheets()
@@ -11,18 +19,36 @@ function myfirsttheme_add_stylesheets()
     '1.0.0',
     'all',
   );
+  /* wp_enqueue_style( */
+  /*   'home-css', */
+  /*   get_template_directory_uri() . '/assets/css/home.css', */
+  /*   [], */
+  /*   '1.0.0', */
+  /*   'all', */
+  /* ); */
+  /// add scripts
+  wp_enqueue_script(
+    'myfirsttheme-script',
+    get_template_directory_uri() . '/assets/js/script.js',
+    [],
+    '1.0.0',
+    [
+      'strategy' => 'defer',
+      'in_footer' => true,
+    ],
+  );
 }
 
 // add sth to head
 add_action('wp_head', 'myfirsttheme_add_this_to_head');
 function myfirsttheme_add_this_to_head()
 {
-  echo "<meta name='author' content='Nawras' />";
+  /* echo "<meta name='author' content='Nawras' />"; */
 }
 
 // add sth to footer
 add_action('wp_footer', 'myfirsttheme_add_this_to_footer');
 function myfirsttheme_add_this_to_footer()
 {
-  echo "<script>console.log('hiiiii')</script>";
+  /* echo "<script>console.log('hiiiii')</script>"; */
 }
