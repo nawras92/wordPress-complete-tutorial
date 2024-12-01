5bc39298ab050d09b6403153545ae3e085e6753c
Nawras <nawras.othman@yandex.com>
2024-12-01 20:32:35 +0300

    enqueue admin scripts

--- plugins/lwn-nawras-menu/lwn-nawras-menu.php
+++ plugins/lwn-nawras-menu/lwn-nawras-menu.php
@@ -4,7 +4,6 @@
  * Plugin Name: Lwn Nawras Admin Menu
  * */
 
-
 add_action('admin_menu', 'lwn_nawras_menu_register_menu');
 function lwn_nawras_menu_register_menu()
 {
@@ -17,4 +16,35 @@ function lwn_nawras_menu_register_menu()
     'dashicons-cover-image',
     240,
   );
+}
+
+// page content callback
+function lwn_nawras_menu_settings_callback()
+{
+  echo 'first page';
+}
+
+// enqueue admin scripts
+add_action('admin_enqueue_scripts', 'lwn_nawras_menu_enqueue_admin_scripts');
+function lwn_nawras_menu_enqueue_admin_scripts($hook)
+{
+  if ($hook == 'toplevel_page_simple-plugin') {
+    wp_enqueue_style(
+      'menu-plugin-style',
+      plugins_url('admin/assets/css/admin.css', __FILE__),
+      [],
+      '1.0.0',
+      'all',
+    );
+
+    wp_enqueue_script(
+      'menu-plugin-script',
+      plugins_url('admin/assets/js/admin-script.js', __FILE__),
+      [],
+      '1.0.0',
+      [
+        'in_footer' => true,
+      ],
+    );
+  }
 }
