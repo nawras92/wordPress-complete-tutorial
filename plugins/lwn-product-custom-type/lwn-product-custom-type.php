017b8f54f891559369ff4a4a7529414ac52a2f25
Nawras <nawras.othman@yandex.com>
2025-01-14 19:38:39 +0300

    add custom taxonomy

--- plugins/lwn-product-custom-type/lwn-product-custom-type.php
+++ plugins/lwn-product-custom-type/lwn-product-custom-type.php
@@ -29,6 +29,7 @@ function lwn_ctp_register_product()
   $args = [
     'labels' => $labels,
     'public' => true,
+    /* 'taxonomies' => ['category', 'post_tag'], */
     'publicly_queryable' => true,
     'show_ui' => true,
     'show_in_menu' => true,
@@ -40,4 +41,29 @@ function lwn_ctp_register_product()
     'menu_position' => 500,
   ];
   register_post_type('lwn-product', $args);
+
+  $tax_labels = [
+    'name' => 'Product Categories',
+    'singular_name' => 'Product Category',
+  ];
+  $tax_args = [
+    'labels' => $tax_labels,
+    'public' => true,
+  ];
+  register_taxonomy('product-category', 'lwn-product', $tax_args);
 }
+
+// register product Taxonomy
+/* add_action('init', 'lwn_ctp_register_product_taxonomy'); */
+/* function lwn_ctp_register_product_taxonomy() */
+/* { */
+/*   $labels = [ */
+/*     'name' => 'Product Categories', */
+/*     'singular_name' => 'Product Category', */
+/*   ]; */
+/*   $args = [ */
+/*     'labels' => $labels, */
+/*     'public' => true, */
+/*   ]; */
+/*   register_taxonomy('product-category', 'lwn-product', $args); */
+/* } */
