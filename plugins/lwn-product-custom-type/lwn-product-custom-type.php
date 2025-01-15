8f2ff59e5d818ce78f16b46dfd04cbb371efc384
Nawras <nawras.othman@yandex.com>
2025-01-15 20:17:30 +0300

    WP Query & get_terms

--- plugins/lwn-product-custom-type/lwn-product-custom-type.php
+++ plugins/lwn-product-custom-type/lwn-product-custom-type.php
@@ -8,6 +8,45 @@ if (!defined('ABSPATH')) {
   exit();
 }
 
+// shortcode
+add_shortcode('lwn-wp-query', 'lwn_ctp_render_shortcode');
+function lwn_ctp_render_shortcode()
+{
+  // ---- Get posts - type = lwn-product: Start
+  $output = '';
+  $args = [
+    'post_type' => 'lwn-product',
+  ];
+  $query = new WP_Query($args);
+  if ($query->have_posts()) {
+    while ($query->have_posts()) {
+      $query->the_post();
+      $output .= get_the_title();
+      $output .= '<br>';
+    }
+    wp_reset_postdata();
+  }
+  return $output;
+
+  // ---- Get posts - type = lwn-product : End
+  // ---- Get terms - product-category: start
+  /* $args = [ */
+  /*   'taxonomy' => 'product-category', // taxonomy name */
+  /*   'hide_empty' => false, */
+  /* ]; */
+  /* $output = ''; */
+  /* $terms = get_terms($args); */
+  /* if (!is_wp_error($terms)) { */
+  /*   foreach ($terms as $term) { */
+  /*     /1* print_r($term); *1/ */
+  /*     $output .= $term->name . ':  ' . $term->term_id; */
+  /*     $output .= '<br>'; */
+  /*   } */
+  /* } */
+  /* return $output; */
+  // ---- Get terms - product-category: end
+}
+
 // register product post
 add_action('init', 'lwn_ctp_register_product');
 function lwn_ctp_register_product()
@@ -30,6 +69,7 @@ function lwn_ctp_register_product()
     'labels' => $labels,
     'public' => true,
     /* 'taxonomies' => ['category', 'post_tag'], */
+    'taxonomies' => ['product-category'],
     'publicly_queryable' => true,
     'show_ui' => true,
     'show_in_menu' => true,
@@ -49,6 +89,7 @@ function lwn_ctp_register_product()
   $tax_args = [
     'labels' => $tax_labels,
     'public' => true,
+    'show_in_rest' => true,
   ];
   register_taxonomy('product-category', 'lwn-product', $tax_args);
 }
