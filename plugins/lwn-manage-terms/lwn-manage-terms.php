30bc1ccf28cf9a479320d8d1474a2d4fa1ec96c4
Nawras <nawras.othman@yandex.com>
2025-01-22 20:02:18 +0300

    update/delete terms

--- plugins/lwn-manage-terms/lwn-manage-terms.php
+++ plugins/lwn-manage-terms/lwn-manage-terms.php
@@ -28,11 +28,23 @@ function lwn_mt_page_cb()
   // Pick a taxonomy: category, post_tag
   $taxonomy = 'post_tag';
 
+  // get terms
+  $terms = get_terms([
+    'taxonomy' => $taxonomy,
+    'hide_empty' => false,
+  ]);
+
   // Process form
   if (isset($_POST['action'])) {
     switch ($_POST['action']) {
       case 'insert_term':
         lwn_mt_insert_term();
+        break;
+      case 'update_term':
+        lwn_mt_update_term();
+        break;
+      case 'delete_term':
+        lwn_mt_delete_term();
         break;
     }
   }
@@ -41,11 +53,44 @@ function lwn_mt_page_cb()
   ?>
 		<div class="wrap">
         <h1 class="wp-heading-inline">Terms Manager</h1>
+         <!-- List terms -->
+          <?php if (!empty($terms) && !is_wp_error($terms)): ?>
+             <h2>List Terms</h2>
+             <ul>
+              <?php foreach ($terms as $term): ?>
+              <li><?php echo esc_html($term->name) .
+                ' (' .
+                esc_html($term->term_id) .
+                ' - ' .
+                esc_html($term->count) .
+                ')'; ?>   </li>
+              <?php endforeach; ?>
+             </ul>
+          <?php else: ?>
+              <p>No terms found </p>
+          <?php endif; ?>
+        
+         <!-- Insert Term -->
          <h2>Insert New Term</h2>
 					<form method="POST">
               <input type="text" name="term_name" required placeholder="enter term name">
                <input type="hidden" name="action" value="insert_term" />
                <input type="submit" value="Insert Term" />
+          </form>
+         <!-- Update Term -->
+         <h2>Update Term</h2>
+					<form method="POST">
+              <input type="text" name="term_id" required placeholder="enter term id">
+              <input type="text" name="new_term_name" required placeholder="enter new term name">
+               <input type="hidden" name="action" value="update_term" />
+               <input type="submit" value="Update Term" />
+          </form>
+         <!-- Delete  Term -->
+         <h2>Delete Term</h2>
+					<form method="POST">
+              <input type="text" name="term_id" required placeholder="enter term id">
+               <input type="hidden" name="action" value="delete_term" />
+               <input type="submit" value="Delete Term" />
           </form>
 
      </div>
@@ -69,6 +114,49 @@ function lwn_mt_insert_term()
     } else {
       echo '<div class="updated"><p>Term inserted successfully: ' .
         esc_html($term_name) .
+        '</p></div>';
+    }
+  }
+}
+
+// Update Handler
+function lwn_mt_update_term()
+{
+  if (isset($_POST['new_term_name']) && isset($_POST['term_id'])) {
+    $term_id = intval($_POST['term_id']);
+    $new_term_name = sanitize_text_field($_POST['new_term_name']);
+    $taxonomy = 'post_tag';
+
+    // Update Term
+    $term = wp_update_term($term_id, $taxonomy, ['name' => $new_term_name]);
+
+    if (is_wp_error($term)) {
+      echo '<div class="error"><p>Error Updating term: ' .
+        $term->get_error_message() .
+        '</p></div>';
+    } else {
+      echo '<div class="updated"><p>Term updated successfully: ' .
+        esc_html($new_term_name) .
+        '</p></div>';
+    }
+  }
+}
+function lwn_mt_delete_term()
+{
+  if (isset($_POST['term_id'])) {
+    $term_id = intval($_POST['term_id']);
+    $taxonomy = 'post_tag';
+
+    // Delete Term
+    $term = wp_delete_term($term_id, $taxonomy);
+
+    if (is_wp_error($term)) {
+      echo '<div class="error"><p>Error deleting term: ' .
+        $term->get_error_message() .
+        '</p></div>';
+    } else {
+      echo '<div class="updated"><p>Term deleted successfully: ' .
+        intval($term_id) .
         '</p></div>';
     }
   }
